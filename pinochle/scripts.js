f151409ade2a8112da8f7932c0f78e78a6f28716
David Salzman <drsalzman@gmail.com>
2024-05-21 16:30:42 -0400

    Touch only

--- pinochle/scripts.js
+++ pinochle/scripts.js
@@ -30,24 +30,34 @@ let picked      = null;
 let handPicked  = -1;
 let cardPicked  = -1;
 
-function bumpCard (e, s, t) {
-    e.preventDefault();
-    s.style.translate = t;
+function enter (event, player, index) {
+    const h = handImage[player][index];
+    const translate = ["20% 0%", "0% 20%", "-20% 0%", "0% -20%"];
+    event.preventDefault();
+    h.style.translate = translate[player];
 }
 
-function playCard (e, s, d) {
-    bumpCard(e, s, "0% 0%");
-    const sx = s.offsetLeft;
-    const sy = s.offsetTop;
-    const dx = d.offsetLeft;
-    const dy = d.offsetTop;
-    s.style.transition = "transform 1s";
-    s.style.transform = "translate(" + (dx-sx) + "px, " + (dy-sy) + "px)";
-    s.ontransitionend = function() {
-        s.style.transition = "transform 0s";
-        s.style.transform = "none";
-        d.src = s.src;
-        s.src = "";
+function leave (event, player, index) {
+    const h = handImage[player][index];
+    event.preventDefault();
+    h.style.translate = "0% 0%";
+}
+
+function press (event, player, index) {
+    const h = handImage[player][index];
+    const l = layImage[player];
+    const hx = Number.parseFloat(getComputedStyle(h).left);
+    const hy = Number.parseFloat(getComputedStyle(h).top);
+    const lx = Number.parseFloat(getComputedStyle(l).left);
+    const ly = Number.parseFloat(getComputedStyle(l).top);
+    event.preventDefault();
+    h.style.transition = "translate 1s";
+    h.style.translate = (lx-hx) + "px " + (ly-hy) + "px";
+    h.ontransitionend = function() {
+        h.style.transition = "translate 0s";
+        h.style.translate = "0px 0px";
+        l.src = h.src;
+        h.src = "";
     }
 }
 
@@ -107,32 +117,6 @@ function slide(event, player, index) {
     }
 }
 
-/*function moveCard(e, side, t) {
-    const cardL = [], cardR = [], cardT = [], cardB = [];
-    const touchX = e.changedTouches[0].clientX;
-    const touchY = e.changedTouches[0].clientY;
-    for (let i = 0; i < side.length; i++) {
-        bumpCard(e, side[i], "0% 0%");
-        cardL[i] = side[i].offsetLeft;
-        cardR[i] = side[i].offsetLeft + side[i].offsetWidth;
-        cardT[i] = side[i].offsetTop;
-        cardB[i] = side[i].offsetTop + side[i].offsetHeight;
-    }
-    if (touchX < cardL[0] || touchX >= cardR.at(-1) || touchY < cardT[0] || touchY >= cardB.at(-1)) {
-        return;
-    }
-    for (let i = 0; i < side.length; i++) {
-        if (touchX >= cardL[i] && touchX < cardR[i] && touchY >= cardT[i] && touchY < cardB[i]) {
-            if (i + 1 < side.length && (side == south || side == north) && touchX >= cardL[i + 1])
-                continue;
-            if (i + 1 < side.length && (side == west || side == east) && touchY >= cardT[i + 1])
-                continue;
-            bumpCard(e, side[i], t);
-            break;
-        }
-    }
-}*/
-
 // initialize javascript after window loads
 window.onload = function() {
     reload.onclick = function() {
@@ -140,20 +124,11 @@ window.onload = function() {
     }
     for (let p = west; p <= south; p++) {
         for (let i = 0; i < dealt; i++) {
-            /*west[i].onmouseenter  = function(e) {bumpCard(e,  west[i],  "20% 0%" );}
-            north[i].onmouseenter = function(e) {bumpCard(e,  north[i], "0% 20%" );}
-            east[i].onmouseenter  = function(e) {bumpCard(e,  east[i],  "-20% 0%");}
-            south[i].onmouseenter = function(e) {bumpCard(e,  south[i], "0% -20%");}
-            west[i].onmouseleave  = function(e) {bumpCard(e,  west[i],  "0% 0%"  );}
-            north[i].onmouseleave = function(e) {bumpCard(e,  north[i], "0% 0%"  );}
-            east[i].onmouseleave  = function(e) {bumpCard(e,  east[i],  "0% 0%"  );}
-            south[i].onmouseleave = function(e) {bumpCard(e,  south[i], "0% 0%"  );}
-            west[i].onmousedown   = function(e) {playCard(e,  west[i],  playWest );}
-            north[i].onmousedown  = function(e) {playCard(e,  north[i], playNorth);}
-            east[i].onmousedown   = function(e) {playCard(e,  east[i],  playEast );}
-            south[i].onmousedown  = function(e) {playCard(e,  south[i], playSouth);}*/
+            //handImage[p][i].onmouseenter = function(e) {enter(e, p, i);}
+            //handImage[p][i].onmouseleave = function(e) {leave(e, p, i);}
+            //handImage[p][i].onmousedown  = function(e) {press(e, p, i);}
             handImage[p][i].ontouchstart = function(e) {touch(e, p, i);}
-            handImage[p][i].ontouchmove  = function(e) {slide(e, p, i);}
+            //handImage[p][i].ontouchmove  = function(e) {slide(e, p, i);}
         }
     }
 }
