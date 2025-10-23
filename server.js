a0a3927af23cab0155c9658189638c9f9cd6fb8d
David Salzman <drsalzman@gmail.com>
2025-10-23 16:30:19 -0400

    Fixed contentType

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ function reqReceived(req, res) {
                 res.writeHead(404, {'Content-Type':'text/html' });
                 res.end();
             } else {
-                res.writeHead(200, { 'Content-Type': contentType });
+                res.writeHead(200, { 'Content-Type': typ });
                 res.end(content, 'utf-8');
             }
         }
